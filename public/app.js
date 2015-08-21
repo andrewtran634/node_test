61b0ddc155a26b1e002993a5e411641dc6d4a16a
Andy Tran <ajtran@umich.edu>
2015-08-20 20:39:21 -0400

    trying to add service?  it is not working

--- public/app.js
+++ public/app.js
@@ -1,17 +1,32 @@
 var app = angular.module('flapperNews', []);
 
+app.factory('posts', [function() {
+	var o = {
+		posts: []
+	};
+	return o;
+}]);
+
 app.controller('MainCtrl', [
 '$scope',
-function($scope){
-	$scope.addPost = function() {
-		$scope.posts.push({title: 'a new post!', 'upvotes' : 0});
-	};
+'posts',
+function($scope, posts){
 	$scope.test = 'Hello world!';
-	$scope.posts = [
-	  	{'title' : 'post1', 'upvotes' : 5}, 
-	  	{'title' : 'post2', 'upvotes' : 2}, 
-	  	{'title' : 'post3', 'upvotes' : 15},
-	  	{'title' : 'post4', 'upvotes' : 9}, 
-	  	{'title' : 'post5', 'upvotes' : 4}
-  	];
+	$scope.posts = posts.posts;
+	$scope.addPost = function() {
+		if(!$scope.title || $scope.title === '') {
+			return;
+		}
+		$scope.posts.push({
+			title: $scope.title, 
+			link: $scope.link,
+			'upvotes' : 0
+		});
+		$scope.title = '';
+		$scope.link = '';
+	};
+	$scope.incrementUpvotes = function(post) {
+		post.upvotes += 1;
+	};
  }]);
+
